3e58d0dc40e36e381e096e7b286acf565c7abb48
Thiago Ladeira <tiladeira@hotmail.com>
2024-04-17 08:45:52 -0300

    Versionamento Front

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'web-food-front/app';
+import config from 'web-food-front/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
